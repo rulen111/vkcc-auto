4691687a2456cb910675b9d04c9defee0e600703
“Ruslan <akhmarovri@yandex.ru>
2024-10-01 15:06:45 +0300

    Make project installable

--- vkcc_auto/config/__init__.py
+++ vkcc_auto/config/__init__.py
@@ -5,7 +5,7 @@ class DefaultConfig(object):
     # VK API access token, can be of any type (user, service, community)
     TOKEN = "1234"
     # The amount of time to wait before sending next request to VK API
-    API_RPS_RATE = 6
+    API_RPS_RATE = 3
 
     # Index of an excel workbook row to be considered first
     # to have relevant data (starts from 1, not 0)
